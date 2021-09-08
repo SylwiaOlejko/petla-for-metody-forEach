3d671ade8846117ae57e277324ac96c919005ad4
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-09-08 17:36:55 +0200

    petla for

--- main.js
+++ main.js
@@ -1,10 +1,24 @@
 // tutaj rozwiązanie
 
-let size = 10; 
+ 
 
-const li = document.createElement("li");
-document.body.appendChild(li);
+const btn = document.querySelector("button");
 
-li.style.display = "block";
-li.style.fontSize = size + "px";
+const liItems = document.querySelectorAll("li");
+let fontSize = 10;
+
+btn.addEventListener("click", () => { //strzałka fat arrow
+
+    console.log("działa");
+    fontSize++;
+    for (let i = 0; i<liItems.length; i++){
+        if (liItems[i])
+        liItems[i].style.display = "block";
+        liItems[i].style.fontSize = `${fontSize}px`;
+        
+    }
+
+
+})
+
 
