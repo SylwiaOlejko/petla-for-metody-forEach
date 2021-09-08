cc88d09a38266044e415e0ae89b1230ce4b3d246
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-09-08 16:00:29 +0200

    zmienna i okreslenie li

---
+++ main.js
@@ -0,0 +1,10 @@
+// tutaj rozwiązanie
+
+let size = 10; 
+
+const li = document.createElement("li");
+document.body.appendChild(li);
+
+li.style.display = "block";
+li.style.fontSize = size + "px";
+
