1f5939edfc4c6677292a615f5a604bf294373007
Sylwia Olejko <sylwia.olejko@gmail.com>
2021-09-27 21:55:21 +0200

    foreach

--- main.js
+++ main.js
@@ -7,22 +7,31 @@ const btn = document.querySelector("button");
 const liItems = document.querySelectorAll("li");
 let fontSize = 10;
 
-btn.addEventListener("click", () => { //strzałka fat arrow
+// btn.addEventListener("click", () => { //strzałka fat arrow
 
-    console.log("działa");
-    fontSize++;
-    for (let i = 0; i<liItems.length; i++){
-        if (liItems[i].style.display === ""){
-            console.log("dodałem block");
-        liItems[i].style.display = "block";
-        }
-        liItems[i].style.fontSize = `${fontSize}px`;
+//     console.log("działa");
+//     fontSize++;
+//     for (let i = 0; i<liItems.length; i++){
+//         // if (liItems[i].style.display === ""){
+//         //     console.log("dodałem block");
+//         // liItems[i].style.display = "block";
+//         // }
+//         liItems[i].style.display = "block";
+//         liItems[i].style.fontSize = `${fontSize}px`;
         
-    }
+//     }
 
 
 
+// })
+
+//FOREACH
+btn.addEventListener("click",() =>{
+   
+    liItems.forEach((liItem) =>{
+        liItem.style.display = "block";
+        liItem.style.fontSize = fontSize + "px";
+});
+    fontSize++;
 })
-
-
 
